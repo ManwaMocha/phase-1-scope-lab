185a450bd4babd94a21f37c61833f947eea3e78b
Augustus Mocha <mocha4pf@gmail.com>
2024-09-30 07:50:59 +0300

    1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName
+customerName = 'bob'
+
+function upperCaseCustomerName (){
+   return customerName=customerName.toUpperCase();
+}
+function setBestCustomer() {
+   return bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'some customer'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another customer';
+}
